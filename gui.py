a97311e564d5772ed30a03cc152f666c21714582
Illés Gergő <illes.gergo1@gmail.com>
2023-07-10 15:14:55 +0200

    gui v1.1

--- gui.py
+++ gui.py
@@ -17,7 +17,23 @@ def matplotCanvas(wavelengths,intensities):
     
     if plotted== False: canvas._tkcanvas.pack(fill=tk.BOTH, expand=True,side=tk.TOP); plotted = True
     
+def matplotCanvasBTN():
+    if backcal.get()==0: intensities = processedData
+    else: intensities = np.subtract(processedData,backcaldata)
+    global plotted, a, f, canvas
+    if plotted==False:
+        f = Figure(figsize=(6,6),dpi=100)
+        a = f.add_subplot(111)
+    if plotted: a.axes.clear()
+    a.plot(spec.wavelengths(),intensities)
+    if plotted == False: canvas = FigureCanvasTkAgg(f,master=plotframe)
+    canvas.draw()
+    if plotted == False: canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True); toolbar=NavigationToolbar2Tk(canvas,plotframe)
+    
+    if plotted== False: canvas._tkcanvas.pack(fill=tk.BOTH, expand=True,side=tk.TOP); plotted = True
+    
 def acqdata():
+    global processedData
     spec.open()
     spec.integration_time_micros(float(timeentry.get())*1e6)
     avgcount = int(avgentry.get())
@@ -53,6 +69,7 @@ def acqbackcalib():
 plotted = False
 spec = sp.Spectrometer.from_serial_number()
 backcaldata = [0]*spec.wavelengths()
+processedData = [0]*spec.wavelengths()
 window = tk.Tk()
 backcal = tk.IntVar()
 title = tk.Label(master=window, text="OceanOptics USB2000+ software by IG",font=16)
@@ -61,9 +78,12 @@ plotframe = tk.Frame(master=window, width=600,height=600)
 plotframe.pack(fill=tk.BOTH,side=tk.LEFT,expand=True)
 setframe = tk.Frame(master=window, width=300,height=600)
 setframe.pack(fill=tk.BOTH,side=tk.LEFT,expand=True)
-plotbutton = tk.Button(master=setframe,text="Plot",font=16,command=acqdata)
+plotbutton = tk.Button(master=setframe,text="Plot",font=16,command=matplotCanvasBTN)
 
-plotbutton.grid(row=6,column=1,columnspan=2,padx=5,pady=6)
+plotbutton.grid(row=6,column=1,columnspan=1,padx=5,pady=6)
+measbutton = tk.Button(master=setframe,text="Measure and plot",font=16,command=acqdata)
+
+measbutton.grid(row=6,column=2,columnspan=1,padx=5,pady=6)
 
 timelabel = tk.Label(master=setframe,text="Integration time (s)",font=16)
 timeentry = tk.Entry(master=setframe,font=16)
